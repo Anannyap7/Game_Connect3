9ff2d611a25aebcb04e7328a696e862c8fce1dcb
Anannyap7 <anannyapopat20@gmail.com>
2021-05-19 20:27:23 +0530

    After debugging

--- app/src/main/java/com/example/game_connect3/MainActivity.java
+++ app/src/main/java/com/example/game_connect3/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.game_connect3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
